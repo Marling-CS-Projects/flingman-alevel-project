e02de8aef19a68b155c134165e2f4da8d920897b
Jack Thorn <15sf33942jt@marling.school>
2021-04-23 11:44:44 +0100

    typscript building

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as _ from 'lodash';
   function component() {
     const element = document.createElement('div');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['TypeScript', 'webpack'], ' ');
 
     return element;
   }
