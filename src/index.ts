2fb1926c1e23ae778162a889251bf9a6898ccec6
Jack Thorn <15sf33942jt@marling.school>
2021-05-04 14:19:04 +0100

    Keyboard controls using p5

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
-import * as _ from 'lodash';
+import * as p5 from 'p5';
 
-  function component() {
-    const element = document.createElement('div');
+let sketch = function(p: p5) {
+  let position:p5.Vector; // has x, y, AND maths for vectors
 
-    element.innerHTML = _.join(['TypeScript', 'webJack'], ' ');
+  p.setup = function() {
+    p.createCanvas(700, 410);
+    position = p.createVector(200, 200);
+  };
 
-    return element;
+  p.draw = function() {
+    p.background(0);
+    p.circle(position.x, position.y, 30);
+  };
+  p.keyPressed = function() {
+    if (p.keyCode === p.LEFT_ARROW) {
+      position.x -= 5;
+    } else if (p.keyCode === p.RIGHT_ARROW) {
+      position.x += 5;
+    }
+    if (p.keyCode === p.UP_ARROW) {
+      position.y -= 5;
+    } else if (p.keyCode === p.DOWN_ARROW) {
+      position.y += 5;
+    }
   }
+};
 
-  document.body.appendChild(component());
+let myp5 = new p5(sketch);
+
