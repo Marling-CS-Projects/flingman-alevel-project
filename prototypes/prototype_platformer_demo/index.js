75e3247ade6107b3bd0c6f2ce716bf0d875dcab9
Jack Thorn <15sf33942jt@marling.school>
2021-05-19 09:08:13 +0100

    working on platformer demo

---
+++ prototypes/prototype_platformer_demo/index.js
@@ -0,0 +1,41 @@
+const app = new PIXI.Application({
+    width: 480,
+    height: 320
+});
+
+const tileSieze = 16;
+
+document.body.appendChild(app.view);
+
+app.loader.add('tileset', 'assets/tileset.png').load((loader, resources) => { 
+
+    let tileTextures = [];
+    for (let i = 0; i < 7 * 11; i++ ) {
+        let x = i % 7;
+        let y = Math.floor(i / 7);
+        tileTextures[i] = new PIXI.Texture(
+            resources.tileset.texture,
+            new PIXI.Rectangle(x * tileSize, x * tileSize, tileSize, tileSize)
+             
+        );
+    }
+    
+    
+    const tileset = new PIXI.Sprite(tileTextures[2]);
+
+    
+
+    tileset.x = app.renderer.width / 2;
+    tileset.y = app.renderer.height / 2;
+
+    tileset.anchor.x = 0.5;
+    tileset.anchor.y = 0.5;
+
+    app.stage.addChild(tileset);
+
+    app.ticker.add(() => {
+        tileset.rotation += 0.01
+    });
+});
+
+app.loader.onError.add((error) => console.error(error));
