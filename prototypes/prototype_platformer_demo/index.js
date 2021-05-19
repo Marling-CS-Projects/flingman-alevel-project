3b07d71bd953ebd32c91a6a821c54a5de8925e76
Jack Thorn <15sf33942jt@marling.school>
2021-05-19 09:51:43 +0100

    terrain on screen!!!

--- prototypes/prototype_platformer_demo/index.js
+++ prototypes/prototype_platformer_demo/index.js
@@ -1,9 +1,28 @@
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+
 const app = new PIXI.Application({
     width: 480,
     height: 320
 });
 
 const tileSize = 16;
+
+let map = {
+    width: 16,
+    height: 10,
+    tiles: [
+        12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+        12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+        12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+        12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+        12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+        12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+        12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+        12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+    ]
+}
 
 document.body.appendChild(app.view);
 
@@ -15,25 +34,43 @@ app.loader.add('tileset', 'assets/tileset.png').load((loader, resources) => {
         let y = Math.floor(i / 7);
         tileTextures[i] = new PIXI.Texture(
             resources.tileset.texture,
-            new PIXI.Rectangle(x * tileSize, x * tileSize, tileSize, tileSize)
+            new PIXI.Rectangle(x * tileSize, y * tileSize, tileSize, tileSize)
         );
     }
 
 
-    const bunny = new PIXI.Sprite(tileTextures[0]);
+    const bunny = new PIXI.Sprite(tileTextures[52]);
+    bunny.scale.x = 4;
+    bunny.scale.y = 4;
 
 
 
     bunny.x = app.renderer.width / 2;
     bunny.y = app.renderer.height / 2;
 
+    
+    let background = new PIXI.Container();
+    for (let y = 0; y < map.width; y++) {
+        for (let x = 0; x < map .width; x++) {
+            let tile = map.tiles[y * map.width + x];
+            let sprite = new PIXI.Sprite(tileTextures[tile]);
+            sprite.x = x * tileSize;
+            sprite.y = y * tileSize;
+            background.addChild(sprite);
+        }
+    }
+
+    background.scale.x = 2;
+    background.scale.y = 2;
+
+
     bunny.anchor.x = 0.5;
     bunny.anchor.y = 0.5;
 
-    app.stage.addChild(bunny);
+    app.stage.addChild(background);
 
     app.ticker.add(() => {
-        tileset.rotation += 0.01
+        bunny.rotation += 0.01
     });
 });
 
